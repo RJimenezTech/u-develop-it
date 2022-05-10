3e2b80011a9baa532fb9425b2d0771a94cecca10
Robert Jimenez <robertjimenez86@gmail.com>
2022-05-10 18:21:30 -0500

    connected db to server

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express');
+
 const PORT = process.env.PORT || 3002;
 const app = express();
+
+const mysql = require('mysql2');
+
+// express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'BadBunnySQL2022!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+)
 
 // request to base server request
 app.get('/', (req,res) => {
@@ -10,7 +28,14 @@ app.get('/', (req,res) => {
         message: 'Hello World'
     });
 });
-// default respone for any other request (Not Found)
+
+// runs the SQL query and executes the callback
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
+// catchall route
+// this route must be last
 app.use((req,res) => {
     res.status(404).end();
 });
